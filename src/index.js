20154fa301a896c48987697eb3ba5f51a58cd489
Magda Bieńkowska <m.lena.bienkowska@gmail.com>
2023-03-30 15:46:23 +0200

    Add new files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
+
+import React from "react";
+import { createRoot } from 'react-dom/client';
+import { Provider } from "react-redux";
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { store } from "./store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    supportedLngs: ['en', 'pl'],
+    fallbackLng: "pl",
+    detection: {
+        order: ['cookie', 'htmlTag', 'localStorage', 'path', 'subdomain'],
+        caches: ['cookie']
+    },
+    backend: {
+        loadPath: '/assets/locales/{{lng}}/translation.json'
+    }
+  });
+
+const el = document.getElementById('root');
+const root = createRoot(el);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <Provider store={store}>
+        <App/>
+    </Provider>
+)
